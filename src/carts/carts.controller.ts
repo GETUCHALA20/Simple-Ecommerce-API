835f9ede9db6565b6a992078dd90308f73191ed1
GETUCHALA20 <ntnlgetu@gmail.com>
2020-11-26 22:06:28 +0300

    cart controller added

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { use } from 'passport';
 import { CartsService } from './carts.service';
+import { CreateCartDTO } from './dto/create-cart.dto';
 
 @ApiTags('Cart')
 @ApiBearerAuth()
@@ -10,12 +12,24 @@ export class CartsController {
 
     @Get(":userId")
     @ApiParam({ name: 'userid', type: String })
-    async cartDetail(@Param('userId') userId){}
+    async cartDetail(@Res() res, @Param('userId') userId){
+        const cart = await this.cartsService.cartDetails(userId);
+        return res.status(HttpStatus.OK).json(cart);
+    }
     
-    @Post('add')
-    async addToCart(){}
+    @Post('addItem')
+    async addToCart(@Res() res, createCartDTO: CreateCartDTO){
+        const userId = createCartDTO.owner;
+        const itemId = createCartDTO.items[0].item;
+        const quantity = parseInt(createCartDTO.items[0].quantity);
+        const cart = await this.cartsService.addToCart(itemId,quantity,userId)
+        return res.status(HttpStatus.OK).json(cart);
+    }
 
-    @Post('remove')
-    async removeFromCart(){}
+    @Delete('removeItem/:itemId')
+    async removeFromCart(@Res() res, @Param('itemId') itemId,@Body() userId){
+        const cart = await this.cartsService.removeItemFromCart(itemId,userId);
+        return res.status(HttpStatus.OK).json(cart);
+    }
 
 }
